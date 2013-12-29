35d019e590ad434092b6480128cfe247578d0f59
tom42 <tom42@server.fake>
2013-12-29 21:11:59 +0100

    Basic textmode .kkapturing in place, works with Lick Weed, Superkiller and The Turing Machines didn't care. The latter requires frame skipping on timeout.

--- kkapturedll/video_console.cpp
+++ kkapturedll/video_console.cpp
@@ -30,29 +30,29 @@ static const int CharHeight = 8;
 
 struct palette_entry
 {
-    unsigned char r;
-    unsigned char g;
-    unsigned char b;
+  unsigned char r;
+  unsigned char g;
+  unsigned char b;
 };
 
 static const palette_entry palette[16] =
 {
-    {0x00, 0x00, 0x00},
-    {0x00, 0x00, 0xaa},
-    {0x00, 0xaa, 0x00},
-    {0x00, 0xaa, 0xaa},
-    {0xaa, 0x00, 0x00},
-    {0xaa, 0x00, 0xaa},
-    {0xaa, 0x55, 0x00},
-    {0xaa, 0xaa, 0xaa},
-    {0x55, 0x55, 0x55},
-    {0x55, 0x55, 0xff},
-    {0x55, 0xff, 0x55},
-    {0x55, 0xff, 0xff},
-    {0xff, 0x55, 0x55},
-    {0xff, 0x55, 0xff},
-    {0xff, 0xff, 0x55},
-    {0xff, 0xff, 0xff}
+  {0x00, 0x00, 0x00},
+  {0x00, 0x00, 0xaa},
+  {0x00, 0xaa, 0x00},
+  {0x00, 0xaa, 0xaa},
+  {0xaa, 0x00, 0x00},
+  {0xaa, 0x00, 0xaa},
+  {0xaa, 0x55, 0x00},
+  {0xaa, 0xaa, 0xaa},
+  {0x55, 0x55, 0x55},
+  {0x55, 0x55, 0xff},
+  {0x55, 0xff, 0x55},
+  {0x55, 0xff, 0xff},
+  {0xff, 0x55, 0x55},
+  {0xff, 0x55, 0xff},
+  {0xff, 0xff, 0x55},
+  {0xff, 0xff, 0xff}
 };
 
 // Draw character onto capture buffer.
@@ -62,56 +62,80 @@ static const palette_entry palette[16] =
 // c     : character that should be rendered
 static void drawCharacter(int column, int row, CHAR_INFO c)
 {
-    const unsigned char* src = &console_font_vga_rom_8x8[unsigned char(c.Char.AsciiChar) * CharHeight];
-    unsigned char* dst = captureData + (column * CharWidth + (captureHeight - 1 - row * CharHeight) * captureWidth) * 3;
-    const palette_entry* fgColor = &palette[c.Attributes & 15];
-    const palette_entry* bgColor = &palette[(c.Attributes >> 4) & 15];
+  const unsigned char* src = &console_font_vga_rom_8x8[unsigned char(c.Char.AsciiChar) * CharHeight];
+  unsigned char* dst = captureData + (column * CharWidth + (captureHeight - 1 - row * CharHeight) * captureWidth) * 3;
+  const palette_entry* fgColor = &palette[c.Attributes & 15];
+  const palette_entry* bgColor = &palette[(c.Attributes >> 4) & 15];
 
-    for (int y = 0; y < CharHeight; ++y)
+  for (int y = 0; y < CharHeight; ++y)
+  {
+    unsigned char bits = *src++;
+    for (int x = 0; x < CharWidth; ++x)
     {
-        unsigned char bits = *src++;
-        for (int x = 0; x < CharWidth; ++x)
-        {
-            const palette_entry* color = (bits & 128) ? fgColor : bgColor;
-            *dst++ = color->b;
-            *dst++ = color->g;
-            *dst++ = color->r;
-            bits <<= 1;
-        }
-        dst -= (captureWidth + CharWidth) * 3;
+      const palette_entry* color = (bits & 128) ? fgColor : bgColor;
+      *dst++ = color->b;
+      *dst++ = color->g;
+      *dst++ = color->r;
+      bits <<= 1;
     }
+    dst -= (captureWidth + CharWidth) * 3;
+  }
+}
+
+static bool isSupportedBlit(const COORD dwBufferSize, const COORD dwBufferCoord, const SMALL_RECT &writeRegion)
+{
+  return (dwBufferSize.X == 80) &&
+    (dwBufferSize.Y == 50) &&
+    (dwBufferCoord.X == 0) &&
+    (dwBufferCoord.Y == 0) &&
+    (writeRegion.Left == 0) &&
+    (writeRegion.Top == 0) &&
+    (writeRegion.Right == 79) &&
+    (writeRegion.Bottom == 49);
+}
+
+static void captureTextmodeFrame(const CHAR_INFO *lpBuffer)
+{
+  setCaptureResolution(80 * CharWidth, 50 * CharHeight);
+  for (int row = 0; row < 50; ++row)
+  {
+    for (int column = 0; column < 80; ++column)
+    {
+      drawCharacter(column, row, *lpBuffer);
+      ++lpBuffer;
+    }
+  }
+  encoder->WriteFrame(captureData);
 }
 
 static BOOL (__stdcall *Real_WriteConsoleOutputA)(HANDLE hConsoleOutput, CONST CHAR_INFO *lpBuffer, COORD dwBufferSize, COORD dwBufferCoord, PSMALL_RECT lpWriteRegion) = WriteConsoleOutputA;
 
 static BOOL __stdcall Mine_WriteConsoleOutputA(HANDLE hConsoleOutput, CONST CHAR_INFO *lpBuffer, COORD dwBufferSize, COORD dwBufferCoord, PSMALL_RECT lpWriteRegion)
 {
-    // TODO: capture (but for the time being, only for known resolution, e.g. 80x50)
-    // TODO: also honor params.CaptureVideo somewhere...
-    auto result = Real_WriteConsoleOutputA(hConsoleOutput, lpBuffer, dwBufferSize, dwBufferCoord, lpWriteRegion);
+  const auto result = Real_WriteConsoleOutputA(hConsoleOutput, lpBuffer, dwBufferSize, dwBufferCoord, lpWriteRegion);
 
-    // TODO: testcode...
-    setCaptureResolution(640, 480);
-    for (int y = 0; y < 16; ++y)
+  if (result && isSupportedBlit(dwBufferSize, dwBufferCoord, *lpWriteRegion))
+  {
+    if (params.CaptureVideo)
     {
-        for (int x = 0; x < 16; ++x)
-        {
-            CHAR_INFO c;
-            c.Char.AsciiChar = x + y * 16;
-            c.Attributes = x;
-            drawCharacter(x, y, c);
-        }
+      captureTextmodeFrame(lpBuffer);
     }
-    encoder->WriteFrame(captureData);
     nextFrame();
+  }
+  else
+  {
+    printLog("video/console: WriteConsoleOutputA: failed/unsupported blit: result=%d dwBufferSize=%d,%d dwBufferCoord=%d,%d *lpWriteRegion=%d,%d,%d,%d\n",
+      result, dwBufferSize.X, dwBufferSize.Y, dwBufferCoord.X, dwBufferCoord.Y,
+      lpWriteRegion->Left, lpWriteRegion->Top, lpWriteRegion->Right, lpWriteRegion->Bottom);
+  }
 
-    return result;
+  return result;
 }
 
 void initVideo_Console()
 {
   if (params.EnableConsoleCapture)
   {
-      HookFunction(&Real_WriteConsoleOutputA, Mine_WriteConsoleOutputA);
+    HookFunction(&Real_WriteConsoleOutputA, Mine_WriteConsoleOutputA);
   }
 }
